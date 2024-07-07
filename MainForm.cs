33c46d8ed226452f7970db63c0a5c42381c92aa8
Dmitry Egorov <mrpromodern@mail.ru>
2024-07-07 13:38:31 +0500

    first form

---
+++ MainForm.cs
@@ -0,0 +1,54 @@
+﻿using PipelineDesign.data;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PipelineDesign
+{
+    public partial class MainForm : Form
+    {
+        private MenuStrip menuStrip;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var db = new DatabaseEntities())
+            {
+                var pipeline = new Pipelines { Name = 1 };
+                db.Pipelines.Add(pipeline);
+                db.SaveChanges();
+            }
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var db = new DatabaseEntities())
+            {
+                var pipeline = db.Pipelines.FirstOrDefault(n => n.Name == 1);
+                if (pipeline == null)
+                {
+                    return;
+                }
+                pipeline.Name = 2;
+                db.SaveChanges();
+            }
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var db = new DatabaseEntities())
+            {
+                var pipelines = db.Pipelines;
+
+                foreach (var pipeline in pipelines)
+                {
+                    MessageBox.Show(pipeline.Name.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ forms/CreateForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace PipelineDesign.forms
+{
+    internal class CreateForm : Form
+    {
+        public CreateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Создание трубопровода";
+            this.ResumeLayout(false);
+        }
+    }
+}
